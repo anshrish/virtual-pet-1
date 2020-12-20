1207073541eec7f0f6f91108e18873b07b1137d2
anshrish <71593063+anshrish@users.noreply.github.com>
2020-12-20 20:33:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var dog, happyDog, database, foodS, foodStock;
+//Create variables here
+
+function preload()
+{
+  dog=loadImage("dogImg.png");
+  dog1=loadImage("dogImg1.png")
+	//load images here
+}
+
+function setup() {
+	createCanvas(500, 500);
+  dog= new Dog 
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
